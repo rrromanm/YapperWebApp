a18d6890098db763c1fabc87df6b3abc945ba911
kacengas <kacengas@gmail.com>
2024-12-02 10:41:45 +0100

    Create post styling

--- SEP3_T2/grpc-springboot/src/main/java/sep3/service/PostImpl.java
+++ SEP3_T2/grpc-springboot/src/main/java/sep3/service/PostImpl.java
@@ -99,7 +99,6 @@ public class PostImpl extends PostServiceGrpc.PostServiceImplBase {
             ArrayList<PostDTO> posts = dao.getAllPosts();
 
             String string = gson.toJson(posts);
-            System.out.println(string);
             GetAllPostsResponse response = GetAllPostsResponse.newBuilder().setList(string).build();
 
             responseObserver.onNext(response);
