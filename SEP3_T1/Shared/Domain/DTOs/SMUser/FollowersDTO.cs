97334468f3953a7279e35e21e987a1f9c27a4365
kacengas <kacengas@gmail.com>
2024-12-04 13:38:41 +0100

    Account recommendation

--- SEP3_T1/Shared/Domain/DTOs/SMUser/FollowersDTO.cs
+++ SEP3_T1/Shared/Domain/DTOs/SMUser/FollowersDTO.cs
@@ -5,4 +5,5 @@ public class FollowersDTO
     public int id { get; set; }
     public string username { get; set; }
     public string nickname { get; set; }
+    public bool IsFollowedByCurrentUser { get; set; }
 }
